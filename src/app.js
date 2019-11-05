f8b05e868b6da33442237b8072dbd983cd5278d9
Ferenc Petöcz <fpetoecz@gmx.de>
2019-11-05 16:36:32 +0100

    refactored to function component

--- src/app.js
+++ src/app.js
@@ -2,27 +2,15 @@ import React from "react";
 import Map from "./map";
 import ReactDOM from "react-dom";
 
-export class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {};
-    }
-
-    componentDidMount() {
-        console.log("App mounted");
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <h1>
-                    S h a d o w <span id="slash">/</span> A r c h i t e c t u r
-                    e
-                </h1>
-                <Map />
-            </React.Fragment>
-        );
-    }
+export default function App() {
+    return (
+        <React.Fragment>
+            <h1>
+                S h a d o w <span id="slash">/</span> A r c h i t e c t u r e
+            </h1>
+            <Map />
+        </React.Fragment>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector("main"));
